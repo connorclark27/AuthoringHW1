af33009c5eef91196c7be3da550230805ab3eeb7
Clark <c_clark20@tss-hr420-dm03.local>
2017-09-25 17:51:13 -0400

    updated lightbox

--- script/main.js
+++ script/main.js
@@ -24,6 +24,11 @@
           newSubImg.classList.add('thumb');
           //set the src
           newSubImg.src = "images/" + objectIndex.images[index];
+
+          //add event handler to trigger a lightbox
+          newSubImg.addEventListener('click', popLightbox, false);
+
+
           //add it to the page
           subImages.appendChild(newSubImg);
         });
@@ -47,4 +52,15 @@
       //add an event handler to each image
       image.addEventListener('click', changeElements, false);
     });
+
+    //trigger the lightbox
+    function popLightbox() {
+      debugger;
+      // trigger the lightbox overlay so that we can see it
+      let lightbox = document.querySelector('.lightbox');
+
+      lightbox.style.display = 'block';
+    }
+
+    changeElements.call(document.querySelector('#spring'));
 })();
